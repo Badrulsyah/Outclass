d7ff655d93c47b857e1f24d051368ce3ead5ec44
Badrulsyah <badrulsyahfirdaus@gmail.com>
2024-11-14 15:19:27 +0800

    Add files via upload

---
+++ sourcecode.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import pickle
+
+st.write("Advertising Sales App")
+st.write("This app predicts the **Sales Value** type!")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    tv = st.sidebar.slider('tv', 0.0, 400.0, 0.0) #(minimum, max, default value)
+    radio = st.sidebar.slider('Radio', 0.0, 400.0, 0.0)
+    newspaper = st.sidebar.slider('Newspaper', 0.0, 200.0, 0.0)
+    
+    data = {'TV': tv,
+            'Radio': radio,
+            'Newspaper': newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+modelsales = pickle.load(open("modelsales.h5", "rb")) #rb: read binary
+new_pred = modelsales.predict(df) # testing (examination)
+
+
+st.subheader('Prediction')
+st.write(new_pred)
+
